4d7787fa7232eac25119308eacef490358092113
Yehor-Havrylenko <egor.dar1216@gmail.com>
2025-06-02 06:43:04 +0200

    Update output.tf

--- Remote/AWS/Terraform/ecr/output.tf
+++ Remote/AWS/Terraform/ecr/output.tf
@@ -1,4 +1,4 @@
-output "repository_url" {
-  description = "URL repository ECR"
-  value       = aws_ecr_repository.repository.repository_url
+output "repository_urls" {
+  description = "Map of repository names to their URLs"
+  value = { for repo_name, repo in aws_ecr_repository.repository : repo_name => repo.repository_url }
 }
