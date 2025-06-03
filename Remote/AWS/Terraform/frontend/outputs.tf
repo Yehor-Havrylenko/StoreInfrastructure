a65929fbfc990a7131b6afc73ba199262982cb97
Yehor-Havrylenko <egor.dar1216@gmail.com>
2025-06-03 11:53:56 +0200

    update frontend output

--- Remote/AWS/Terraform/frontend/outputs.tf
+++ Remote/AWS/Terraform/frontend/outputs.tf
@@ -6,7 +6,7 @@ output "bucket_regional_domain_name" {
   value       = aws_s3_bucket.this.bucket_regional_domain_name
 }
 
-output "cloudfront_domain_name" {
+/*output "cloudfront_domain_name" {
   description = "domain name CloudFront Distribution"
   value       = aws_cloudfront_distribution.frontend_distribution.domain_name
-}
+}*/
